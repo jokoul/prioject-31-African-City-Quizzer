71220657367fc69babc9a8ba60bed3be7de3a570
jokoul <74833985+jokoul@users.noreply.github.com>
2022-06-26 12:12:17 +0200

    Build User Interface

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>African City Quizzer</title>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1>African City Quizzer</h1>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <h2>Test Your African Geography Knowledge</h2>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+               FOOTER To change later
+            </div>
+        </footer>
+    </body>
+</html>
